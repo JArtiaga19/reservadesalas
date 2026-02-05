42569aee220fa1991b5a4c7ed8938a7b14ea9203
Jesus <ja3@gmail.com>
2026-02-05 10:35:16 +0100

    primer commit

---
+++ reservasalas/src/test/java/com/jesus/reservasalasapi/ReservasalasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesus.reservasalasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasalasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
